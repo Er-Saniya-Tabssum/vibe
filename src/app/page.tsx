f5ca98843aad25c871e90c76dd3afc5299f80c53
Er-Saniya-Tabssum <saniyatabssum23.set@modyuniversity.ac.in>
2025-08-02 20:38:39 +0530

    03: trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-const Page = () => {
+// "use client";
+import { Suspense } from "react";
 
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { Client } from "./client";
+// import { useTRPC } from "@/trpc/client";
+// import { useQuery } from "@tanstack/react-query";
+
+const Page = async() => {
+  // const trpc = useTRPC();
+  // trpc.CreateAI.queryOptions({ text: 123 });
+  // const { data } = useQuery(trpc.CreateAI.queryOptions({ text: "John" }));
+  // const  data  = await caller.CreateAI({ text: "Antonio SERVER" })
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.CreateAI.queryOptions({ text: "John PREFETCH" }));
   return (
-    <div>
-      Hello World!!
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+    <Suspense fallback={<p>Loading...</p>}>
+    <Client />
+    </Suspense>
+    </HydrationBoundary>
   );
 }
 
